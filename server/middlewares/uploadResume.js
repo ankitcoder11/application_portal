c45755d490b45098c117667446889682efa6882e
ankitcoder11 <ankitakcp@gmail.com>
2025-08-06 18:18:36 +0530

    deploying issue solved

--- server/middlewares/uploadResume.js
+++ server/middlewares/uploadResume.js
@@ -1,7 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { cloudinary } from "../utiles/cloudinary.js";
-
+import { cloudinary } from "../utiles/Cloudinary.js";
 const resumeStorage = new CloudinaryStorage({
   cloudinary,
   params: (req, file) => {
